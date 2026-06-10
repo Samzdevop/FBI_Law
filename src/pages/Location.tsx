5dae7c4acd4f0291323b2eaba58b65e4e4f582ad
Chukwuma Samuel <168547650+Samzdevop@users.noreply.github.com>
2026-06-09 21:13:10 -0700

    second commit

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -5,28 +5,27 @@ import Footer from "../components/footer/Footer";
 const locations = [
   {
     title: "ASA Law Group Servicing Employers in Illinois",
-    heading: "OUR ILLINOIS LOCATION",
+    heading: "Canada",
     address: [
-      "1301 W 22ND ST",
-      "SUITE 201",
-      "OAK BROOK, IL 60523",
+      "3525 Platinum Dr. ",
+      " Mississauga ON",
+      "L5M 2R9",
     ],
-    phone: "630.819.8344",
-    fax: "630.214.0077",
-    description: `ASA Law Group, LLC was founded in Oak Brook, Illinois with the mission to partner with employers in not only the defense of workers compensation and civil cases, but also as legal consultants in advancing the employer's overall mission and corporate goals. We work closely with employers throughout the state of Illinois in handling of cases by using multifaceted metrics that serve as not only performance measures but also to detect algorithms that help identify mitigation opportunities, cost control and case closures. We fully service employers that are self-insured, mid-size as well as small businesses. We are proud to continue to service employers throughout the state in an unparalleled fashion!`,
+    // phone: "630.819.8344",
+    // fax: "630.214.0077",
+    description: `ASP Law Group, LLC was founded in L5M 2R9, Canada with the mission to partner with employers in not only the defense of workers compensation and civil cases, but also as legal consultants in advancing the employer's overall mission and corporate goals. We work closely with employers throughout the state of Illinois in handling of cases by using multifaceted metrics that serve as not only performance measures but also to detect algorithms that help identify mitigation opportunities, cost control and case closures. We fully service employers that are self-insured, mid-size as well as small businesses. We are proud to continue to service employers throughout the state in an unparalleled fashion!`,
   },
   {
     title: "ASA Law Group Servicing Employers in California",
-    heading: "OUR CALIFORNIA LOCATION",
+    heading: "Canada",
     address: [
-      "4510 EAST PACIFIC",
-      "COAST HIGHWAY",
-      "SUITE 595",
-      "LONG BEACH, CA 90804",
+     "3525 Platinum Dr. ",
+      " Mississauga ON",
+      "L5M 2R9",
     ],
-    phone: "310.793.6598",
-    fax: "562.684.0378",
-    description: `Based upon client demand, ASA Law Group, Inc. also services employers in northern and southern California in defense of workers compensation and civil litigation. We are able to successfully implement our unique approach to case handling within the legal and business environment in California. Notably, we use multifaceted metrics that are specific to California and partner with employers to not only handle legal matters but also serve as consultants in advising employers in a competitive business market. We look forward in becoming your legal partners!`,
+    // phone: "310.793.6598",
+    // fax: "562.684.0378",
+    description: `Based upon client demand, ASP Law Group, Inc. also services employers in northern and southern California in defense of workers compensation and civil litigation. We are able to successfully implement our unique approach to case handling within the legal and business environment in California. Notably, we use multifaceted metrics that are specific to California and partner with employers to not only handle legal matters but also serve as consultants in advising employers in a competitive business market. We look forward in becoming your legal partners!`,
   },
 ];
 
@@ -53,8 +52,6 @@ export default function Location() {
 
                     {locations.map((location, index) => (
                         <div key={index}>
-
-                        {/* Header Card */}
                         <div className="bg-[#353535]">
                             <div
                             className="h-4"
